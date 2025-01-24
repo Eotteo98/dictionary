589086d64a483618f9ad96093a88d995c38fdfd1
Eotteo98 <cosma.teo@yahoo.com>
2025-01-24 18:55:27 +0100

    Add files via upload

--- s.py
+++ s.py
@@ -7,23 +7,23 @@ HEIGHT = 600
 TITLE = "shoot_the_alien"
 message = ""
 
-alien = Actor("alien")
-alien.pos = 0,0
+alien2 = Actor("alien2")
+alien2.pos = 0,0
 
 def draw():
     screen.clear()
-    screen.fill("black")
-    alien.draw()
+    screen.fill("white")
+    alien2.draw()
     screen.draw.text(message, center = (200,20), fontsize = 25)
 
     
 def rand():
-    alien.x = random.randint(10,WIDTH-10)
-    alien.y = random.randint(10, HEIGHT-10)
+    alien2.x = random.randint(10,WIDTH-10)
+    alien2.y = random.randint(10, HEIGHT-10)
 
 def on_mouse_down(pos):
     global message 
-    if alien.collidepoint(pos):
+    if alien2.collidepoint(pos):
         message = "Good shot"
         rand()
     else:
